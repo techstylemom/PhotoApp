db0e57cb9802504082c6e7a2a16876f250d01a01
irish <techstylemom@gmail.com>
2021-08-27 08:10:34 +0300

    Initial Commit

---
+++ photoapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  photoapp
+//
+//  Created by Irish on 8/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
